22f2784ceded9758c22c837dd01bdfed6a00655d
sv709 <sv709@yahoo.com>
2025-07-02 11:11:39 -0700

    add mockk dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -97,6 +97,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.8.0-RC2")
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:1.9.23")
+    testImplementation("io.mockk:mockk:1.14.4")
     androidTestImplementation("androidx.core:core-animation-testing:1.0.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
